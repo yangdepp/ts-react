2981a2007bfe09e999f9e50797673ab4f7cde475
yangdepp <yangdepp@163.com>
2020-07-07 21:06:28 +0800

    fix wran

--- base-practice/src/page/useEffect/demo3.tsx
+++ base-practice/src/page/useEffect/demo3.tsx
@@ -10,17 +10,18 @@ const Demo3: FC = () => {
       setCount((pre) => pre + 1);
     }, 1000);
     return () => {
-      console.log('444')
+      console.log('444');
       window.clearInterval(timer);
       document.title = `已卸载${count}`;
     };
+    // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
     console.log('111');
     document.title = `update${count}`;
   }, [count]);
-  console.log('render')
+  console.log('render');
   return (
     <div>
       <div>Count: {count}</div>
